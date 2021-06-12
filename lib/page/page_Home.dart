69de51212da39ac34ce468eee8a982a4150a1e7b
EnVISSE <erik.kantona8@gmail.com>
2021-06-12 19:01:29 +0800

    add sha1

--- lib/page/page_Home.dart
+++ lib/page/page_Home.dart
@@ -3,6 +3,7 @@ import 'package:flutter_firebase_auth/service/authentication_service.dart';
 import 'package:provider/provider.dart';
 
 class HomePage extends StatelessWidget {
+  final field1 = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,7 +12,22 @@ class HomePage extends StatelessWidget {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text('Home Page'),
+              TextFormField(
+                controller: field1,
+                decoration: InputDecoration(
+                    labelText: 'Email', border: OutlineInputBorder()),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => Page2(
+                            text1: field1.text,
+                          ),
+                        ));
+                  },
+                  child: Text('Go to second page')),
               ElevatedButton(
                   onPressed: () {
                     context.read<AuthenticationService>().signout();
@@ -20,6 +36,20 @@ class HomePage extends StatelessWidget {
             ],
           ),
         ),
+      ),
+    );
+  }
+}
+
+class Page2 extends StatelessWidget {
+  final String text1;
+  Page2({this.text1});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text(text1),
       ),
     );
   }
